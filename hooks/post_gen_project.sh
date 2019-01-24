1bb45042b30044b6a2976eede6226378e151dcd0
jmcarpenter2 <jcarpenter@manifold.ai>
2019-01-24 12:13:47 -0800

    temporary printing test

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,6 +6,10 @@ num=$2
 awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
 }
 
+
+echo {{ cookiecutter.github_API_key }}
+
+
 # Initial sync of data folder to project specific prefix under the manifold-projects bucket
 echo "Creating shared data folders at s3://manifold-projects/{{ cookiecutter.repo_name }}..."
 aws s3 sync data s3://manifold-projects/{{ cookiecutter.repo_name }} 
